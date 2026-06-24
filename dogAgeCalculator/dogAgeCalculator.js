0b026a200c8c952d91f51852d98764c39d726863
LottaMonet <lotta@anniecannons.com>
2026-06-24 14:30:32 -0700

    update dogAgeCalculator
    added logic for the dog age calculator and tip calculator

--- dogAgeCalculator/dogAgeCalculator.js
+++ dogAgeCalculator/dogAgeCalculator.js
@@ -8,3 +8,28 @@ Run this file with the node command:
 node dogAgeCalculator.js
 
 */
+
+const dogName = process.argv[2];
+const dogAge = Number(process.argv[3]);
+
+if (!dogName || !process.argv[3]) {
+console.log("Please enter a dog's name and age.");
+} else if (isNaN(dogAge)) {
+console.log("Please enter a number for the dog's age.");
+} else if (dogAge <= 0) {
+console.log("Please enter a valid age for your dog.");
+} else if (!Number.isInteger(dogAge)) {
+console.log("Please enter a whole number for the dog's age.");
+} else {
+let dogYears;
+
+if (dogAge === 1) {
+dogYears = 15;
+} else if (dogAge === 2) {
+dogYears = 24;
+} else {
+dogYears = 24 + (dogAge - 2) * 5;
+}
+
+console.log(`Your dog, ${dogName}, is ${dogAge} years old, but that's ${dogYears} years old in dog years!`);
+}
--- lecture/tipCalculator.js
+++ lecture/tipCalculator.js
@@ -6,3 +6,25 @@
 let bill = 100
 let tipPercentage = 0.20
 let numGuests = 4
+
+// Let's make a tip calculator with Node.js!
+
+// Run this file with the node command:
+// node tipCalculator.js
+
+
+let bill = Number(process.argv[2]);
+let tipPercentage = Number(process.argv[3]);
+let numGuests = Number(process.argv[4]);
+
+// calclulate the tip amount
+let tipAmount = bill * tipPercentage;
+console.log("tip amount:", tipAmount);
+
+// calculate the total bill
+let totalBill = bill + tipAmount;
+console.log("total bill:", totalBill);
+
+// calculate the total bill for each guest
+let amountOwedPerGuest = totalBill / numGuests;
+console.log("each guest will owe:", amountOwedPerGuest);
